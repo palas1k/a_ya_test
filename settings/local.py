264b05bdc8d6b1f5473b496d43310843188c7219
palas1k <liserg008@gmail.com>
2024-09-07 12:16:39 +0300

    add local

---
+++ settings/local.py
@@ -0,0 +1,19 @@
+# DJANGO
+from . import db
+# DATABASES
+from .core import DATABASES  # noqa
+DATABASES['default']['PASSWORD'] = db.PASSWORD
+
+# DEBUG
+# https://docs.djangoproject.com/en/3.2/ref/settings/#debug
+from .core import TEMPLATES  # noqa
+DEBUG = True
+INTERNAL_IPS = ('127.0.0.1', 'localhost')
+TEMPLATES[0]['OPTIONS'].update({'debug': DEBUG})
+ALLOWED_HOSTS = ['*']
+
+# EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# ****************************************************************
+# THIRD-PARTY
